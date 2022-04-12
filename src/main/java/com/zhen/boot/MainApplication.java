52402df468b341862794bcfda63c584aa3ef5213
Zhen <eternal9908xx@gmail.com>
2022-04-12 22:40:53 +0800

    Test2

--- src/main/java/com/zhen/boot/MainApplication.java
+++ src/main/java/com/zhen/boot/MainApplication.java
@@ -3,6 +3,7 @@ package com.zhen.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//git test
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
